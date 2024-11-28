18144e6885c2bc793af1bed4c57b98d95badc728
Liam Jordan Ndjock <104268627+jordanNdjock@users.noreply.github.com>
2024-11-28 13:39:35 +0100

    finish next course

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,24 @@
-export default function Page(){
-    return <p>Customers Page</p>
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page(props: {
+    searchParams?: Promise<{
+      query?: string;
+      page?: string;
+    }>;
+  }) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchFilteredCustomers(query);
+    return <main>
+        <CustomersTable query={query} currentPage={currentPage}/>
+        <div className="mt-5 flex w-full justify-center">
+      </div>
+    </main>
 }
